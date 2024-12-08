00e7b22f34a185dc4543e347f8de96bee38b09e3
vivekarza <vivek.arza@gmail.com>
2024-12-08 15:07:54 -0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+/* export default defineConfig({
+  plugins: [react()],
+}) */
+
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  preview: {
+   port: 80,
+   strictPort: true,
+  },
+  server: {
+   port: 80,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:80",
+  },
+ });
